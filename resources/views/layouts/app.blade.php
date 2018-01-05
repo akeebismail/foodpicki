9f90d8f82b81c2ab3ef93a587c2db41975220de9
Akeeb Ismail <damiz.akeeb@gmail.com>
2018-01-05 00:51:09 +0000

    start app page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,73 +8,33 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} - @yield('title') </title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{asset('shop/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('shop/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('shop/css/animsition.min.css')}}" rel="stylesheet">
+    <link href="{{asset('shop/css/animate.css')}}" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="{{asset('shop/css/style.css')}}" rel="stylesheet">
+    @yield('stylesheet')
+
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
-    </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <!--/end:Site wrapper -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="{{asset('shop/js/jquery.min.js')}}"></script>
+    <script src="{{asset('shop/js/tether.min.js')}}"></script>
+    <script src="{{asset('shop/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('shop/js/animsition.min.js')}}"></script>
+    <script src="{{asset('shop/js/bootstrap-slider.min.')}}js"></script>
+    <script src="{{asset('shop/js/jquery.isotope.min.js')}}"></script>
+    <script src="{{asset('shop/js/headroom.js')}}"></script>
+    <script src="{{asset('shop/js/foodpicky.min.js')}}"></script>
+    @yield('jscript')
 </body>
 </html>
